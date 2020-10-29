c285b65df6218594a392870cd308ef11ee93745f
Gabriel <perien.op@gmail.com>
2020-10-29 18:49:05 -0300

    Update Controller
    Controller Classes criado e construido

---
+++ backend/src/routes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+
+const routes = express.Router();
+const classesControllers = new ClassesController();
+
+routes.post('/classes', classesControllers.create);
+routes.get('/classes', classesControllers.index);
+
+export default routes;
